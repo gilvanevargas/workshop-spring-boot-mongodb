38d2a454fc2df41d9a5b4462388b0be8bf262376
Gilvane Vargas <gilvane.vargas@gmail.com>
2021-03-17 22:30:22 -0300

    Consulta simples com query methods

--- src/main/java/com/katon/workshopmongo/services/PostService.java
+++ src/main/java/com/katon/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.katon.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,8 @@ public class PostService {
 	    Optional<Post> user = repo.findById(id);
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
 	}
-
+	
+	public List<Post> findBtTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
