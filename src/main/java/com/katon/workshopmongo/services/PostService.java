11f1a213b0ca71ade5555f0e62334efc1c5972c8
Gilvane Vargas <gilvane.vargas@gmail.com>
2021-03-17 21:59:19 -0300

    Obtendo um post por id

---
+++ src/main/java/com/katon/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.katon.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.katon.workshopmongo.domain.Post;
+import com.katon.workshopmongo.repository.PostRepository;
+import com.katon.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+
+}
